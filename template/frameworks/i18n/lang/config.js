952c00e58da7d06dddd77045644ce049c5236df9
Matteo Rigon <matteo.rigon@atoms.studio>
2020-02-27 11:05:06 +0100

    Add nuxt-i18n to modules

---
+++ template/frameworks/i18n/lang/config.js
@@ -0,0 +1,13 @@
+export default {
+  locales: [
+    {
+      code: 'en',
+      file: 'en.js',
+      iso: 'en'
+    }
+  ],
+  defaultLocale: 'en',
+  seo: true,
+  lazy: true,
+  langDir: 'lang/'
+}
